464f978c2cb75723397a0e1977163242b68d9fa4
Tirsa St. Fort <lifelovelori@gmail.com>
2023-03-05 16:53:50 -0800

    Completed lab

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseString(word)
+  return word === reversedWord
+  }
+
+function reverseString(word) {
+  return word.split("").reverse().join("")
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+  
+ 
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
